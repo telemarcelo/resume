88afb9130a550ccf9d6eb7b23fb2fb6780a982bf
telemarcelo <telemarcelo@gmail.com>
2016-07-28 14:53:07 -0400

    here I added a lot of improvements to images (picking random, moving, fitting) as well as adding formatting and finishing content.  Somehow I lost map functionality here.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,16 +2,17 @@ var bio = {
 	"name" : "Marcelo Antunes", 
 	"role" : "Math/CS Teacher",
 	"contact" : {
-		"mobile" : "941-735-1174",
-		"email" : "telemarcelo@gmail.com",
-		"github" : "telemarcelo",
-		"twitter" : "telemarcelo",
- 		"location" : "Hefei, China"
+		"mobile" : " 941-735-1174",
+		"email" : " telemarcelo@gmail.com",
+		"github" : " telemarcelo",
+		"twitter" : " telemarcelo",
+ 		"location" : " Hefei, China"
 	},
-	"welcomeMessage": "   Dedicated CS/Math Teacher with a strong desire to communicate the beauty and importance of the new literacy.<BR>   An amateur artist and musician who would like to use these skills as a vehicle for teaching technology.<BR>",
+	"welcomeMessage_1": "Dedicated CS/Math Teacher with a strong desire to communicate the beauty and importance of the new literacy.<BR>",
+	"welcomeMessage_2": "An amateur artist and musician who would like to use these skills as a vehicle for teaching technology.<BR><BR>",
 	"biopic" : "images/portrait-small.jpg",
 	"skills" : ['Teaching','Programming','Web Design', 'Game Design','Risk Management',
-	,'Portuguese', 'Soccer Coaching']
+	,'Languages', 'Soccer Coaching']
 };
 
 bio.display = function(){
@@ -28,14 +29,17 @@ bio.display = function(){
 	contacts.push(HTMLlocation.replace("%data%", bio.contact.location));
 	for(index in contacts){
 		$('#topContacts').append(contacts[index]);
+		$('#footerContacts').append(contacts[index]);
 	}
 
 	var formattedPic = HTMLbioPic.replace("%data%",bio.biopic);
 	$('#header').append(formattedPic);
 
-	var formattedMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+	var formattedMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage_1);
 	$('#header').append("<p></p>" + formattedMessage);
-
+	var formattedMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage_2);
+	$('#header').append("<p></p>" + formattedMessage);
+	
 	if(bio.skills.length > 0){
 		$("#header").append(HTMLskillsStart);
 		for(index in bio.skills){
@@ -49,16 +53,16 @@ var education = {
 	"schools": [{
 		"name": "Harvard University",
 		"location": "Cambridge, MA",
-		"degree": "Master of Science, 1996",
+		"degree": "Master of Science",
 		"major": "Applied Mathematics",
-		"dates" : "1995-1996", 
+		"dates" : "1996", 
 		"url" : "?"
 	},{
 		"name": "Harvard College",
 		"location": "Cambridge, MA",
-		"degree": "Bachelor of Arts, 1994",
+		"degree": "Bachelor of Arts",
 		"major": "Applied Mathematics",
-		"dates" : "1995-1996", 
+		"dates" : "1994", 
 		"url" : "?"
 	}],
 	"professional": [{
@@ -66,10 +70,10 @@ var education = {
 		"degree": "Associate of the Society of Actuaries, 2007"
 	}],
 	"online": [{
-		"title": "Udacity",
+		"title": "Front-End Web Developer",
 		"School": "Udacity",
-		"dates": "Nanodegree: 300hrs, currently enrolled",
-		"URL": "Front-End Web Developer"
+		"dates": "2016",
+		"URL": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
 	}]
 };
 
@@ -79,7 +83,26 @@ education.display = function(){
 		$("#education").append(HTMLschoolStart);
 		for(index in education.schools){
 			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[index].name);
-			$(".education-entry:last").append(formattedSchoolName);
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[index].degree);
+			$(".education-entry:last").append(formattedSchoolName+formattedSchoolDegree);
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[index].dates);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[index].location);
+			$(".education-entry:last").append(formattedSchoolDates+formattedSchoolLocation);
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[index].major);
+			$(".education-entry:last").append(formattedSchoolMajor);
+		}
+	}
+
+	if(education.online.length > 0){
+		$(".education-entry:last").append(HTMLonlineClasses);
+		for(index in education.schools){
+			var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.online[index].title);
+			var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.online[index].School);
+			$(".education-entry:last").append(formattedOnlineTitle+formattedOnlineSchool);
+			var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.online[index].dates);
+			$(".education-entry:last").append(formattedOnlineDates);
+			var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.online[index].URL);
+			$(".education-entry:last").append(formattedOnlineURL);
 		}
 	}
 }
@@ -88,13 +111,13 @@ education.display = function(){
 var work = {
 	"jobs":[{
 		"employer" : "Hefei No. 1 High School",
-		"title" : "Calculus Teacher",
+		"title" : "Calculus Instructor",
 		"location" : "Hefei, China",
 		"dates" : "9/1/2016 - Future",
 		"description" : "Teach four sections of AP Calculus"
 	},{
 		"employer" : "Saint Stephen’s Episcopal School",
-		"title" : "Computer Science Teacher",
+		"title" : "Computer Science Instructor",
 		"location" : "Bradenton, FL",
 		"dates" : "8/15/2014 - 12/31/2015",
 		"description" : "Taught AP Computer Science A, Honors Computer Science, and Intro to Computer Science"
@@ -115,7 +138,7 @@ var work = {
 		"title" : "Senior Actuarial Analyst",
 		"location" : "Louisville, KY",
 		"dates" : "5/1/2004 - 5/1/2007",
-		"description" : "Risk Reporting: tasks included S&P Capital Modeling, Municipal Contract cash flow projections, liquidity modeling, and other project and risk reports.  Valuation Area: Tasks included Cash Flow Testing, Embedded Value, and Loss Recognition. Numerous other projects to expand business and protect firm’s capital."
+		"description" : "Risk Reporting: tasks included S&P Capital Modeling, Municipal Contract cash flow projections, liquidity modeling, and other project and risk reports.  <br>Valuation Area: Tasks included Cash Flow Testing, Embedded Value, and Loss Recognition. Numerous other projects to expand business and protect firm’s capital."
 	}]}
 
 work.display = function(){
@@ -124,7 +147,13 @@ work.display = function(){
 		$("#workExperience").append(HTMLworkStart);
 		for(index in work.jobs){
 			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[index].employer);
-			$(".work-entry:last").append(formattedEmployer);
+			var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[index].title);
+			$(".work-entry:last").append(formattedEmployer + formattedTitle);
+			var formattedDate =  HTMLworkDates.replace("%data%", work.jobs[index].dates);
+			var formattedLocation =  HTMLworkLocation.replace("%data%", work.jobs[index].location);
+			$(".work-entry:last").append(formattedDate + formattedLocation);
+			var formattedDescription =  HTMLworkDescription.replace("%data%", work.jobs[index].description);
+			$(".work-entry:last").append(formattedDescription);
 		}
 	}
 }
@@ -132,20 +161,20 @@ work.display = function(){
 
 var projects = {
 	"projects":[{
-		"title" : "Computer Science Teacher",
+		"title" : "Mr. Brainstorms",
 		"dates" : "8/15/2014 - 12/31/2015",
-		"description" : "blah!",
-		"image-url" : "blueh!"
+		"description" : "Website for programming and robotics educational programs for kids.",
+		"image_url" : "images/MrBrainstorms-small.jpg"
 	},{
-		"title" : "Computer Science Teacher",
+		"title" : "Eyepublic",
 		"dates" : "8/15/2014 - 12/31/2015",
-		"description" : "blah!",
-		"image-url" : "blueh!"
+		"description" : "A website to store my online resume, projects, and hobbies.",
+		"image_url" : "images/Eyepublic-small.jpg"
 	},{
-		"title" : "Computer Science Teacher",
+		"title" : "MarCuban",
 		"dates" : "8/15/2014 - 12/31/2015",
-		"description" : "blah!",
-		"image-url" : "blueh!"
+		"description" : "A frogger like game where Marc Cuban has to transport rum from Cuba <br>to Kingston without being arrested by the Trump Coast Guard.",
+		"image_url" : "images/MarCuban-small.jpg"
 	}],
 }
 
@@ -154,18 +183,28 @@ projects.display = function(){
 		$("#projects").append(HTMLprojectStart);
 		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[index].title);
 		$(".project-entry:last").append(formattedProjectTitle);
+		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[index].dates);
+		$(".project-entry:last").append(formattedProjectDates);
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[index].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+		var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[index].image_url);
+		$(".project-entry:last").append(formattedProjectImage);
 	}
+	$(".project-entry:last").append("<br>");
+	$(".project-entry:last").append("<br>");
 }
 
 
 bio.display();
 projects.display();
+work.display();
 education.display();
+
 //var projects = {"projects":[ title, dates worked, description, img URL]}
 
 
 
-work.display();
+
 
 function locationizer(work_obj) {
     var locations = [];
